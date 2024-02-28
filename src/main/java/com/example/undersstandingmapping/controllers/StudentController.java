0dc89edcff6c7725acebbd76051a771677847034
Akash15121992 <akashkumar1528@outlook.com>
2024-02-28 12:30:06 +0530

    APi Changes

--- src/main/java/com/example/undersstandingmapping/controllers/StudentController.java
+++ src/main/java/com/example/undersstandingmapping/controllers/StudentController.java
@@ -3,8 +3,7 @@ package com.example.undersstandingmapping.controllers;
 import com.example.undersstandingmapping.models.Student;
 import com.example.undersstandingmapping.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +28,24 @@ public class StudentController {
     }
 
     // Transactions 1 and 2 completely.
+    //create an api which allows you to create a student via postman and record is inserted in db
+    @RequestMapping(method = RequestMethod.PUT,value = "/create")
+    public Student createStudent(@RequestBody Student student){
 
+    return  studentRepository.save(student);
+    }
+
+    //create another api , which allows you to delete a student by taking id
+
+    @RequestMapping(method = RequestMethod.DELETE,value = "/delete/{id}")
+    public void deleteStudent(@PathVariable Long id){
+
+        studentRepository.deleteById(id);
+    }
+
+    //create another api using which we can search a student by name
+    @RequestMapping(method = RequestMethod.GET,value = "/getStudent")
+    public Student getStudent(@RequestParam String name){
+        return  studentRepository.findByName(name);
+    }
 }
